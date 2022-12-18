d584ad493cf28736b6d441485d7b877902a6f12e
Greg Rychlewski <greg.rychlewski@gmail.com>
2022-12-18 00:59:31 -0500

    mix.exs

--- mix.exs
+++ mix.exs
@@ -1,12 +1,18 @@
 defmodule PostgrexTextExt.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/greg-rychlewski/postgrex_text_ext"
+  @version "0.1.0"
+
   def project do
     [
       app: :postgrex_text_ext,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.6",
-      start_permanent: Mix.env() == :prod,
+      name: "PostgrexTextExt",
+      description: "Text Extensions for Postgrex",
+      docs: docs(),
+      package: package(),
       deps: deps()
     ]
   end
@@ -23,6 +29,25 @@ defmodule PostgrexTextExt.MixProject do
     [
       {:ex_doc, ">= 0.0.0", only: :docs},
       {:postgrex, ">= 0.0.0"}
+    ]
+  end
+
+  defp docs do
+    [
+      source_url: @source_url,
+      source_ref: "v#{@version}",
+      main: "readme",
+      extras: ["README.md", "CHANGELOG.md"],
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Greg Rychlewski"],
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => @source_url},
+      files:
+        ~w(.formatter.exs mix.exs README.md CHANGELOG.md lib)
     ]
   end
 end
